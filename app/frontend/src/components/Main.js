e930e8c7a50923eb4ab2a35deb705d86c952ffc0
andrewrreed <areed1242@gmail.com>
2020-12-15 11:53:04 -0500

    add source header to frontend assets

--- app/frontend/src/components/Main.js
+++ app/frontend/src/components/Main.js
@@ -1,79 +1,102 @@
+/*
+ * ****************************************************************************
+ *
+ *  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
+ *  (C) Cloudera, Inc. 2020
+ *  All rights reserved.
+ *
+ *  Applicable Open Source License: Apache 2.0
+ *
+ *  NOTE: Cloudera open source products are modular software products
+ *  made up of hundreds of individual components, each of which was
+ *  individually copyrighted.  Each Cloudera open source product is a
+ *  collective work under U.S. Copyright Law. Your license to use the
+ *  collective work is as provided in your written agreement with
+ *  Cloudera.  Used apart from the collective work, this file is
+ *  licensed for your use pursuant to the open source license
+ *  identified above.
+ *
+ *  This code is provided to you pursuant a written agreement with
+ *  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+ *  this code. If you do not have a written agreement with Cloudera nor
+ *  with an authorized and properly licensed third party, you do not
+ *  have any rights to access nor to use this code.
+ *
+ *  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+ *  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+ *  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+ *  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+ *  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+ *  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+ *  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+ *  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+ *  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+ *  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+ *  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+ *  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+ *  DATA.
+ *
+ * ***************************************************************************
+ */
+
 import React, { Component } from "react";
-import {
-    Route,
-    HashRouter,
+import { Route, HashRouter } from "react-router-dom";
 
-} from "react-router-dom";
-
-
-import "./main.css"
+import "./main.css";
 
 import GroupView from "./groupview/GroupView";
 import AppHeader from "./header/Header";
-import { createBrowserHistory } from 'history';
-
-
+import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory({
-    basename: "", // The base URL of the app (see below)
-    forceRefresh: false, // Set true to force full page refreshes
-    keyLength: 6, // The length of location.key
-    // A function to use to confirm navigation with the user (see below)
-    getUserConfirmation: (message, callback) => callback(window.confirm(message))
+  basename: "", // The base URL of the app (see below)
+  forceRefresh: false, // Set true to force full page refreshes
+  keyLength: 6, // The length of location.key
+  // A function to use to confirm navigation with the user (see below)
+  getUserConfirmation: (message, callback) => callback(window.confirm(message)),
 });
 
-history.listen(location => {
+history.listen((location) => {
+  // console.log(location.pathname, location.hash)
+});
 
-    // console.log(location.pathname, location.hash)
-})
-
-let linkHolder = {}
+let linkHolder = {};
 
 function updateLh(location) {
-
-    if (location.hash in linkHolder) {
-        linkHolder[location.hash] = linkHolder[location.hash] + 1
-    } else {
-        linkHolder[location.hash] = 0
-    }
-
+  if (location.hash in linkHolder) {
+    linkHolder[location.hash] = linkHolder[location.hash] + 1;
+  } else {
+    linkHolder[location.hash] = 0;
+  }
 }
 
-history.listen(location => {
-    updateLh(location)
+history.listen((location) => {
+  updateLh(location);
 });
 
-
 class Main extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {};
+    updateLh(window.location);
+  }
 
-        this.state = {
-        }
-        updateLh(window.location)
-    }
+  componentDidMount() {}
+  render() {
+    return (
+      <HashRouter>
+        <AppHeader></AppHeader>
 
-    componentDidMount() {
-
-    }
-    render() {
-
-
-        return (
-            <HashRouter>
-                <AppHeader></AppHeader>
-
-                <main className="container-fluid p10">
-                    <Route exact path="/" component={GroupView} />
-                    {/* <Route exact path="/train" component={Train} /> */}
-
-                </main>
-                {/* <div id="footer"> <Footer /> </div> */}
-            </HashRouter>
-
-        );
-    }
+        <main className="container-fluid p10">
+          <Route exact path="/" component={GroupView} />
+          {/* <Route exact path="/train" component={Train} /> */}
+        </main>
+        {/* <div id="footer"> <Footer /> </div> */}
+      </HashRouter>
+    );
+  }
 }
 
 export default Main;
